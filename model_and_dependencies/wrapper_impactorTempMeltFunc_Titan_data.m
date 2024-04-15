a85903d33a841ca973f37cbb6624f64263a7c0a2
Afzal Shadab <mashadab@mit.edu>
2024-04-15 12:17:48 -0500

    Adding lid

--- model_and_dependencies/wrapper_impactorTempMeltFunc_Titan_data.m
+++ model_and_dependencies/wrapper_impactorTempMeltFunc_Titan_data.m
@@ -6,7 +6,7 @@ cox_and_bauer_impact_key = 'Wakita-Stokes';
 basal_viscosity = 1e14; % basal viscosity of ice
 viscosity_activation_energy = 50e3; % viscosity activation energy
 %% model run simulation
-simpleporositywavetest(cox_and_bauer_impact_key,basal_viscosity, ...
+impactorTempMeltFuncDSModPresForm2StokesTitandataShigeru(cox_and_bauer_impact_key,basal_viscosity, ...
     viscosity_activation_energy) %main driver routine
 
 %impactorTempMeltFuncTitan_data : original Stokes code with Titan data
