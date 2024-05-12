2bba9af901f89bfc7052677c409b14eed5177c6f
Afzal Shadab <mashadab@mit.edu>
2024-05-11 22:39:36 -0500

    drainage contour

--- model_and_dependencies/fig1_tempPhiMeltDrain.m
+++ model_and_dependencies/fig1_tempPhiMeltDrain.m
@@ -2,18 +2,19 @@
 set(groot,'defaulttextinterpreter','latex')
 set(groot,'defaultAxesTickLabelInterpreter','latex')
 set(groot,'defaultLegendInterpreter','latex')
-set(groot,'defaultTextFontSize',11)
+set(groot,'defaultTextFontSize',20)
+set(groot, 'DefaultAxesFontSize', 20);
 addpath ../model_and_dependencies/
 clear all; close all;
 fs = 18;
 %% set up grid
 Xmax = 20; %maximum 
 Ymax = 60; %maximum  
-grRes = 100; % grid resolution in radial direction
-grZ = 300; % grid resolution in radial direction
+grRes = 50; % grid resolution in radial direction
+grZ = 180; % grid resolution in radial direction
 d = 20;
 Gridp.xmin = 0; Gridp.xmax = Xmax/d; Gridp.Nx = grRes; %radial direction
-Gridp.ymin = 0; Gridp.ymax = Ymax/d;  Gridp.Ny = grZ; %vertical direction
+Gridp.ymin = -3/5; Gridp.ymax = Ymax/d;  Gridp.Ny = grZ; %vertical direction
 Gridp.geom = 'cylindrical_rz';
 Grid = build_stokes_grid_cyl(Gridp);
 [X,Y] = meshgrid(Grid.p.xc,Grid.p.yc);
@@ -21,10 +22,9 @@ Grid = build_stokes_grid_cyl(Gridp);
 
 %% make plot dimensions
 fp = '../Output/';
-inds2 = [100,2000,4000,6000,8000];
-
-inds1 = [100,3000,6000,9000,17100];
-
+inds2 = [1,5000,10000,30000,42000]; %Darcy-Stokes
+inds1 = [1,2500,3000,4500,5500];%Stokes
+caption = 0; %0 = no caption, 1=caption
 
 inds = [inds1; inds2];
 
@@ -44,10 +44,10 @@ tFrame = zeros(2,5);
 for i = 1:2
     for j = 1:5
         if i ==1 %Close to Stokes
-            load(sprintf("../Output/Shigeru_impact_Wakita-Stokes_eta0_14kc5.6e-16_Ea_50_output_%sC.mat",num2str(inds1(j))))
+            load(sprintf("../Output/Simple-test_eta0_14kc1.85e-10_Ea_50_output_%sC.mat",num2str(inds1(j))))
         end
         if i==2  %Close to Darcy-Stokes
-            load(sprintf("../Output/Shigeru_impact_Wakita-Stokes_eta0_14kc5.6e-11_Ea_50_output_%sC.mat",num2str(inds2(j))))
+            load(sprintf("../Output/Simple-test_eta0_14kc1.85e-08_Ea_50_output_%sC.mat",num2str(inds2(j))))
         end
         %load([fp fns{i} '/i' num2str(inds(i,j)) '.mat'],'T','phi','tVec');
         TPlot = reshape(T,Grid.p.Ny,Grid.p.Nx)*173+100;
@@ -55,14 +55,15 @@ for i = 1:2
 
 %         TPlot(phiPlot > 1e-2) = nan;
         phiPlot(phiPlot < 1e-16) = nan;
-        
+        TPlot(TPlot>273)=273; %%%%%new
+        TPlot(TPlot<94)=94; %%%%%new
         axs{i,j} = subplot(4,5,j+(i-1)*10);
         hold on
         contourf(X*d,Y*d,TPlot,40,'linestyle','none');
-        caxis([100 273]);
+        caxis([94 273]);
         axis equal
         xlim([0 20]);
-        ylim([-1 60]);
+        ylim([0 60]);
         axs{i,j}.XTickLabel = [];
         axs{i,j}.YTickLabel = [];
         axs{i,j}.Units = 'centimeters';
@@ -86,7 +87,7 @@ for i = 1:2
         contourf(X*d,Y*d,phiPlot,40,'linestyle','none');
         axis equal
         xlim([0 20]);
-        ylim([-1 60]);
+        ylim([0 60]);
         axs{i,5+j}.XTickLabel = [];
         axs{i,5+j}.YTickLabel = [];
         axs{i,5+j}.Units = 'centimeters';
@@ -105,7 +106,7 @@ for i = 1:2
                 cs{4} = c;
             end
         end
-        tFrame(i,j) = round(tVec(end),-1);
+        tFrame(i,j) = round(tVec(end),2);
         
     end
 end
@@ -142,8 +143,8 @@ axs{i,j}.YTick = tik2;
 axs{i,j}.YTickLabel = yTikLab;
 axs{i,j}.XTick = tik1;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.YLabel.String = 'z-dir, km';
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.YLabel.String = 'z-dir [km]';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 
 axs{i,j}.YLabel.FontSize = fs;
 axs{i,j}.XLabel.FontSize = fs;
@@ -153,7 +154,7 @@ j = 1;
 % axs{i,j}.Position = [lMarg,bMarg+wid+vMarg1,wid,wid];
 axs{i,j}.YTick = tik2;
 axs{i,j}.YTickLabel = yTikLab;
-axs{i,j}.YLabel.String = 'z-dir, km';
+axs{i,j}.YLabel.String = 'z-dir [km]';
 
 axs{i,j}.YLabel.FontSize = fs;
 
@@ -163,7 +164,7 @@ j = 1;
 % axs{i,j}.Position = [lMarg,bMarg+2*(wid+vMarg1)+vMarg2,wid,wid];
 axs{i,j}.YTick = tik2;
 axs{i,j}.YTickLabel = yTikLab;
-axs{i,j}.YLabel.String = 'z-dir, km';
+axs{i,j}.YLabel.String = 'z-dir [km]';
 
 axs{i,j}.YLabel.FontSize = fs;
 
@@ -172,7 +173,7 @@ j = 6;
 % axs{i,j}.Position = [lMarg,bMarg+3*(wid+vMarg1)+vMarg2,wid,wid];
 axs{i,j}.YTick = tik2;
 axs{i,j}.YTickLabel = yTikLab;
-axs{i,j}.YLabel.String = 'z-dir, km';
+axs{i,j}.YLabel.String = 'z-dir [km]';
 
 axs{i,j}.YLabel.FontSize = fs;
 
@@ -190,6 +191,7 @@ for i = 1:5
     axs{1,i+5}.Visible = 'off';
     axs{2,i+5}.Visible = 'off';
     set([axs{1,i},axs{1,i+5}],'Position',axs{1,i}.Position)
+    if caption == 1
     text(axs{2,i+5},19,0,[num2str(tFrame(2,i)) ' yrs'],...
         'FontSize',fs,'HorizontalAlignment','right','Color',[1,0,0])
     text(axs{1,i+5},19,0,[num2str(tFrame(1,i)) ' yrs'],...
@@ -198,6 +200,7 @@ for i = 1:5
         'FontSize',fs,'HorizontalAlignment','right','Color',[1,0,0])
     text(axs{2,i+5},19,9.9,figLab(i+5),'fontweight','bold','interpreter','tex',...
         'FontSize',fs,'HorizontalAlignment','right','Color',[1,0,0])
+    end
 end
 
 
@@ -205,7 +208,7 @@ i = 2;
 j = 1;
 axs{i,j}.XTick = tik1;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 
@@ -214,7 +217,7 @@ j = 2;
 axs{i,j}.XTick = tik1;
 axs{i,j}.YTick = tik2;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 
@@ -223,7 +226,7 @@ j = 3;
 axs{i,j}.XTick = tik1;
 axs{i,j}.YTick = tik2;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 i = 2;
@@ -231,7 +234,7 @@ j = 4;
 axs{i,j}.XTick = tik1;
 axs{i,j}.YTick = tik2;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 i = 2;
@@ -239,7 +242,7 @@ j = 5;
 axs{i,j}.XTick = tik1;
 axs{i,j}.YTick = tik2;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 i = 1;
@@ -270,22 +273,11 @@ saveas(gcf,[Filename '.pdf']);
 
 
 
-
-
-
-
-
-
-
-
-
-%Stokes vs 1e-13 case
-%% make plot dimensions
+%% make plot dimensions = mid kc = 1e-12 m^2
 fp = '../Output/';
-inds2 = [100,10000,20000,30000,38400];
 
-inds1 = [100,3000,6000,9000,17100];
-
+inds1 = [1,3000,6000,11000,70000];%Stokes
+inds2 = [1,3000,6000,11000,70000];%Stokes
 
 inds = [inds1; inds2];
 
@@ -305,10 +297,10 @@ tFrame = zeros(2,5);
 for i = 1:2
     for j = 1:5
         if i ==1 %Close to Stokes
-            load(sprintf("../Output/Shigeru_impact_Wakita-Stokes_eta0_14kc5.6e-16_Ea_50_output_%sC.mat",num2str(inds1(j))))
+            load(sprintf("../Output/Simple-test_eta0_14kc1.85e-16_Ea_50_output_%sC.mat",num2str(inds1(j))))
         end
         if i==2  %Close to Darcy-Stokes
-            load(sprintf("../Output/Shigeru_impact_Wakita-Stokes_eta0_14kc5.6e-13_Ea_50_output_%sC.mat",num2str(inds2(j))))
+            load(sprintf("../Output/Simple-test_eta0_14kc1.85e-16_Ea_50_output_%sC.mat",num2str(inds2(j))))
         end
         %load([fp fns{i} '/i' num2str(inds(i,j)) '.mat'],'T','phi','tVec');
         TPlot = reshape(T,Grid.p.Ny,Grid.p.Nx)*173+100;
@@ -316,14 +308,15 @@ for i = 1:2
 
 %         TPlot(phiPlot > 1e-2) = nan;
         phiPlot(phiPlot < 1e-16) = nan;
-        
+        TPlot(TPlot>273)=273; %%%%%new
+        TPlot(TPlot<94)=94; %%%%%new
         axs{i,j} = subplot(4,5,j+(i-1)*10);
         hold on
         contourf(X*d,Y*d,TPlot,40,'linestyle','none');
-        caxis([100 273]);
+        caxis([94 273]);
         axis equal
         xlim([0 20]);
-        ylim([-1 60]);
+        ylim([0 60]);
         axs{i,j}.XTickLabel = [];
         axs{i,j}.YTickLabel = [];
         axs{i,j}.Units = 'centimeters';
@@ -347,7 +340,7 @@ for i = 1:2
         contourf(X*d,Y*d,phiPlot,40,'linestyle','none');
         axis equal
         xlim([0 20]);
-        ylim([-1 60]);
+        ylim([0 60]);
         axs{i,5+j}.XTickLabel = [];
         axs{i,5+j}.YTickLabel = [];
         axs{i,5+j}.Units = 'centimeters';
@@ -366,7 +359,7 @@ for i = 1:2
                 cs{4} = c;
             end
         end
-        tFrame(i,j) = round(tVec(end),-1);
+        tFrame(i,j) = round(tVec(end),2);
         
     end
 end
@@ -403,8 +396,8 @@ axs{i,j}.YTick = tik2;
 axs{i,j}.YTickLabel = yTikLab;
 axs{i,j}.XTick = tik1;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.YLabel.String = 'z-dir, km';
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.YLabel.String = 'z-dir [km]';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 
 axs{i,j}.YLabel.FontSize = fs;
 axs{i,j}.XLabel.FontSize = fs;
@@ -414,7 +407,7 @@ j = 1;
 % axs{i,j}.Position = [lMarg,bMarg+wid+vMarg1,wid,wid];
 axs{i,j}.YTick = tik2;
 axs{i,j}.YTickLabel = yTikLab;
-axs{i,j}.YLabel.String = 'z-dir, km';
+axs{i,j}.YLabel.String = 'z-dir [km]';
 
 axs{i,j}.YLabel.FontSize = fs;
 
@@ -424,7 +417,7 @@ j = 1;
 % axs{i,j}.Position = [lMarg,bMarg+2*(wid+vMarg1)+vMarg2,wid,wid];
 axs{i,j}.YTick = tik2;
 axs{i,j}.YTickLabel = yTikLab;
-axs{i,j}.YLabel.String = 'z-dir, km';
+axs{i,j}.YLabel.String = 'z-dir [km]';
 
 axs{i,j}.YLabel.FontSize = fs;
 
@@ -433,7 +426,7 @@ j = 6;
 % axs{i,j}.Position = [lMarg,bMarg+3*(wid+vMarg1)+vMarg2,wid,wid];
 axs{i,j}.YTick = tik2;
 axs{i,j}.YTickLabel = yTikLab;
-axs{i,j}.YLabel.String = 'z-dir, km';
+axs{i,j}.YLabel.String = 'z-dir [km]';
 
 axs{i,j}.YLabel.FontSize = fs;
 
@@ -451,6 +444,7 @@ for i = 1:5
     axs{1,i+5}.Visible = 'off';
     axs{2,i+5}.Visible = 'off';
     set([axs{1,i},axs{1,i+5}],'Position',axs{1,i}.Position)
+        if caption == 1
     text(axs{2,i+5},19,0,[num2str(tFrame(2,i)) ' yrs'],...
         'FontSize',fs,'HorizontalAlignment','right','Color',[1,0,0])
     text(axs{1,i+5},19,0,[num2str(tFrame(1,i)) ' yrs'],...
@@ -459,6 +453,7 @@ for i = 1:5
         'FontSize',fs,'HorizontalAlignment','right','Color',[1,0,0])
     text(axs{2,i+5},19,9.9,figLab(i+5),'fontweight','bold','interpreter','tex',...
         'FontSize',fs,'HorizontalAlignment','right','Color',[1,0,0])
+    end
 end
 
 
@@ -466,7 +461,7 @@ i = 2;
 j = 1;
 axs{i,j}.XTick = tik1;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 
@@ -475,7 +470,7 @@ j = 2;
 axs{i,j}.XTick = tik1;
 axs{i,j}.YTick = tik2;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 
@@ -484,7 +479,7 @@ j = 3;
 axs{i,j}.XTick = tik1;
 axs{i,j}.YTick = tik2;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 i = 2;
@@ -492,7 +487,7 @@ j = 4;
 axs{i,j}.XTick = tik1;
 axs{i,j}.YTick = tik2;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 i = 2;
@@ -500,7 +495,7 @@ j = 5;
 axs{i,j}.XTick = tik1;
 axs{i,j}.YTick = tik2;
 axs{i,j}.XTickLabel = xTikLab;
-axs{i,j}.XLabel.String = 'radius, km';
+axs{i,j}.XLabel.String = 'Rad. [km]';
 axs{i,j}.XLabel.FontSize = fs;
 
 i = 1;
@@ -524,6 +519,7 @@ axs{i,j}.XTick = tik1;
 axs{i,j}.YTick = tik2;
 
 f.PaperPosition = [0.0177,4.6440,4.4646,5.7121];
-Filename='FinalStokesvs1e-13'
+Filename='FinalStokesvsDarcyStokesmid'
 set(gcf,'PaperPositionMode','auto'); 
 saveas(gcf,[Filename '.pdf']);
+
